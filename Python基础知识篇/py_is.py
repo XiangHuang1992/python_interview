a564db611ce94bcff32d57e8bbc9db03c5428800
huangxiang <huangxiang1992@hotmail.com>
2018-12-18 14:33:23 +0800

    python中的is和==

--- Python基础知识篇/py_is.py
+++ Python基础知识篇/py_is.py
@@ -1,30 +1,92 @@
 a1 = 5
 b1 = 5
 print(id(a1), id(b1))
-print(a1 is b1) # True
-print(a1==b1)  # True
+print(a1 is b1)  # True
+print(a1 == b1)  # True
 
-a2 = {'name':'huang', 'age':17}
-b2 = {'name':'huang', 'age':17}
+a2 = {'name': 'huang', 'age': 17}
+b2 = {'name': 'huang', 'age': 17}
 print(id(a2), id(b2))
-print(a2 is b2) # False
-print(a2==b2) # True
+print(a2 is b2)  # False
+print(a2 == b2)  # True
 
 a3 = [1, 2, 3, 4, 5]
 b3 = [1, 2, 3, 4, 5]
 print(id(a3), id(b3))
-print(a3 is b3) # False
-print(a3==b3) # True
+print(a3 is b3)  # False
+print(a3 == b3)  # True
 
-a4 = (1,2,3,4,5)
-b4 = (1,2,3,4,5)
+a4 = (1, 2, 3, 4, 5)
+b4 = (1, 2, 3, 4, 5)
 print(id(a4), id(b4))
-print(a4 is b4) # False
-print(a4==b4) # True
+print(a4 is b4)  # False
+print(a4 == b4)  # True
 
 a5 = 'huang'
 b5 = 'huang'
 print(id(a5), id(b5))
-print(a5 is b5) #True
-print(a5==b5) #True
+print(a5 is b5)  #True
+print(a5 == b5)  #True
 
+import os
+a1 = 5
+b1 = 5
+print(id(a1), id(b1))
+print(a1 is b1)  # True
+print(a1 == b1)  # True
+
+a2 = {'name': 'huang', 'age': 17}
+b2 = {'name': 'huang', 'age': 17}
+print(id(a2), id(b2))
+print(a2 is b2)  # False
+print(a2 == b2)  # True
+
+a3 = [1, 2, 3, 4, 5]
+b3 = [1, 2, 3, 4, 5]
+print(id(a3), id(b3))
+print(a3 is b3)  # False
+print(a3 == b3)  # True
+
+a4 = (1, 2, 3, 4, 5)
+b4 = (1, 2, 3, 4, 5)
+print(id(a4), id(b4))
+print(a4 is b4)  # False
+print(a4 == b4)  # True
+
+a5 = 'huang'
+b5 = 'huang'
+print(id(a5), id(b5))
+print(a5 is b5)  #True
+print(a5 == b5)  #True
+
+import os
+a1 = 5
+b1 = 5
+print(id(a1), id(b1))
+print(a1 is b1)  # True
+print(a1 == b1)  # True
+
+a2 = {'name': 'huang', 'age': 17}
+b2 = {'name': 'huang', 'age': 17}
+print(id(a2), id(b2))
+print(a2 is b2)  # False
+print(a2 == b2)  # True
+
+a3 = [1, 2, 3, 4, 5]
+b3 = [1, 2, 3, 4, 5]
+print(id(a3), id(b3))
+print(a3 is b3)  # False
+print(a3 == b3)  # True
+
+a4 = (1, 2, 3, 4, 5)
+b4 = (1, 2, 3, 4, 5)
+print(id(a4), id(b4))
+print(a4 is b4)  # False
+print(a4 == b4)  # True
+
+a5 = 'huang'
+b5 = 'huang'
+print(id(a5), id(b5))
+print(a5 is b5)  # True
+print(a5 == b5)  # True
+print(os)
